57f6d89b0445d8ed6f01b81b160c9af9b1c27bf1
ZozAhmad <100292735+ZozAhmad@users.noreply.github.com>
2022-12-15 12:20:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -44,9 +44,8 @@ const gameOver = () => {
     element.style.backgroundColor = 'white'
   })
   window.alert("Game Over! You got a score of "+(clickedids.length-1))
-      var obj = {"video": {
-      "value": "<iframe title='YouTube video player' type=\"text/html\" width='640' height='390' src='https://www.youtube.com/watch?v=dQw4w9WgXcQ' frameborder='0' allowFullScreen></iframe>"
-}}
-document.write(obj.video.value);
+<script>
+  document.getElementById('header').innerHTML = "<iframe title='YouTube video player'   type=\'text/html\' width='640' height='390' src='http://www.youtube.com/embed/W-Q7RMpINVo' frameborder='0' allowFullScreen></iframe>"
+</script>
 }
 
